590c61ff78296bcad6f7514f096d4c762fd1a8a1
Najeebahmed11 <43084585+Najeebahmed11@users.noreply.github.com>
2020-05-20 18:15:49 +0500

    Completed Lesson 33. Clean up and saving our code to source control

---
+++ client-app/src/Caritem.tsx
@@ -0,0 +1,17 @@
+import  React from 'react'
+import { ICar } from './demo'
+interface IProps{
+    car : ICar
+}
+
+const Caritem: React.FC<IProps> = ({car}) => {
+    return (
+        <div>
+            <h1>
+                {
+                    car.color
+                }
+            </h1>
+        </div>
+    )
+}
