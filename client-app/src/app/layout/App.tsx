289963f575c71b514972bb73013ef5d88d2f6ee6
Najeebahmed11 <43084585+Najeebahmed11@users.noreply.github.com>
2020-05-23 09:43:31 +0500

    Completed Lesson 54. Refactoring our class component to use React Hooks

--- client-app/src/app/layout/App.tsx
+++ client-app/src/app/layout/App.tsx
@@ -1,55 +1,41 @@
-import  React,{Component} from 'react';
+import  React,{useState,useEffect} from 'react';
 
 import { Header, Icon, List } from 'semantic-ui-react'
 import axios from 'axios'
 import { IActivity } from '../models/activity';
-interface IState{
-  activities : IActivity[]
-}
-class App extends Component<{},IState> {
-  readonly state : IState={
-    activities:[]
 
-  }
-componentDidMount() {
-  axios.get<IActivity[]>('http://locolhost:5000/api/activities')
-    .then((response)=>{
-   //   console.log(response);
-      this.setState({
-        activities: response.data
-      })
+const App =()=> {
+  const [activities,setActivities]=useState<IActivity[]>([])
+  useEffect(()=>{
+    //componentDidMount() {
+    axios
+      .get<IActivity[]>('http://locolhost:5000/api/activities')
+      .then((response)=>{
+      //console.log(response);
+        setActivities(response.data)
+      });
+  });
 
-//inntro
+  return (
+    <div >
+      <Header as='h2'>
+        <Icon name='users' />
+        <Header.Content>Reactivities</Header.Content>
+      </Header>
 
-
-    })
-  
-
-
-}
-
-  render(){
-
-    return (
-      <div >
-        <Header as='h2'>
-    <Icon name='users' />
-    <Header.Content>Reactivities</Header.Content>
-  </Header>
-
-  <List>
-  {this.state.activities.map((activity)=>(
+      <List>
+        {activities.map((activity)=>(
            <List.Item key={activity.id}>{activity.title}</List.Item>
-          ))}
+          ),[])}
     
-  </List>
+      </List>
 
 
 
         
-      </div>
-    );
-  }
+    </div>
+  );
+  
   
 }
 
